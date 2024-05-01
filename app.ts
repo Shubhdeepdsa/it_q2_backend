d1dec4f6acfb0f20422d43b1a732a115d99d2541
Shubhdeep das <90149745+Shubhdeepdsa@users.noreply.github.com>
2024-05-01 18:55:26 +0530

    Something

--- app.ts
+++ app.ts
@@ -1,12 +1,9 @@
-// Import required modules
 import * as dotenv from 'dotenv';
 import { Application, Request, Response } from 'express';
 import { Pool, QueryResult } from 'pg';
 import express = require('express')
-// Load environment variables from .env file
 dotenv.config();
 
-// PostgreSQL configuration
 const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD } = process.env;
 const pool = new Pool({
     host: PGHOST,
@@ -20,12 +17,10 @@ const pool = new Pool({
 });
 
 const app: Application = express();
-app.use(express.json()); // Middleware to parse JSON request body
-
+app.use(express.json()); 
 app.get('/ping', (req, res) => {
     res.send('Pong!');
 });
-// Start the Express.js server
 const PORT: number = parseInt(process.env.PORT || '5000');
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
